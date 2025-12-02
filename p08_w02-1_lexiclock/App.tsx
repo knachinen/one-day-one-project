7622b29c58ba2de2546ddd8932c642ac772dda38
knachinen <knachinen@gmail.com>
2025-12-02 13:43:22 +0900

    mod: fix. conflicting packages

--- p08_w02-1_lexiclock/App.tsx
+++ p08_w02-1_lexiclock/App.tsx
@@ -1,10 +1,16 @@
-import { MainScreen } from './src/screens/MainScreen';
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import MainScreen from './src/screens/MainScreen';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <MainScreen />
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <StatusBar style="auto" />
+        <MainScreen />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
