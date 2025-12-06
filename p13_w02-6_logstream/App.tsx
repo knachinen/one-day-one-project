2b6957a1794d8cdddee070018cfce11efe5acc9e
knachinen <knachinen@gmail.com>
2025-12-06 12:42:50 +0900

    [13-02-6] mod: device status bar color

--- p13_w02-6_logstream/App.tsx
+++ p13_w02-6_logstream/App.tsx
@@ -1,10 +1,15 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { AppNavigator } from './src/navigation/AppNavigator';
-import { useLogStream } from './src/hooks/useLogStream';
+import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
+import React from "react";
+import { AppNavigator } from "./src/navigation/AppNavigator";
+import { useLogStream } from "./src/hooks/useLogStream";
 
 export default function App() {
   useLogStream();
-  return <AppNavigator />;
+  return (
+    <>
+      <AppNavigator />
+      <StatusBar style="dark" />
+    </>
+  );
 }
-
