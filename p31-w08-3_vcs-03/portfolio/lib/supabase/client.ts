3b14ab5fda4d6343d6c149bcbe595f72bc0be4cf
knachinen <knachinen@gmail.com>
2026-01-19 23:00:40 +0900

    [31-08-3] mod: fix. middleware

--- p31-w08-3_vcs-03/portfolio/lib/supabase/client.ts
+++ p31-w08-3_vcs-03/portfolio/lib/supabase/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
@@ -7,7 +7,7 @@ if (!supabaseUrl || !supabaseKey) {
   console.warn('Supabase credentials are not provided in environment variables.');
 }
 
-export const supabase = createClient(
+export const supabase = createBrowserClient(
   supabaseUrl,
   supabaseKey
 )
